098eddd064f65af37048e96087ebab7846cd9633
VikashPR <vikashrajendran2806@gmail.com>
2024-10-07 09:28:28 +0000

    Refactor bootstrap.js: Update mount function to generate fake products dynamically

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -2,9 +2,24 @@ import faker from "faker";
 
 let products = "";
 
-for (let i = 0; i < 3; i++) {
+const mount = (el) => {
+  let products = "";
+
+  for (let i = 0; i < 3; i++) {
     const name = faker.commerce.productName();
-    products += `<div>${name}</div>`
+    products += `<div>${name}</div>`;
+  }
+    // ReactDOM.render(<App />, el);
+    el.innerHTML = products;
+};
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-products");
+
+  // Assuming our container doesn't have an element
+  if (el) {
+    mount(el);
+  }
 }
 
-document.querySelector("#dev-products").innerHTML = products;
+export { mount };
